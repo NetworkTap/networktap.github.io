e5f0c8c4d40851c4686c232afeaf9966b125f858
prathgan <prathamgandhi4@gmail.com>
2017-12-11 14:31:45 -0500

    working on login verification

---
+++ js/login.js
@@ -0,0 +1,22 @@
+function login(){
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    console.log(email);
+    console.log(password);
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    if(error){
+        console.log("login failed")
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        document.getElementById("error").innerHTML = errorMessage;
+    }
+    else{
+        document.getElementById("error").innerHTML = " ";
+        console.log("logged in");
+        window.location.assign("../html/profile.html");
+    }
+    });
+}
+function testFunc(){
+    console.log("js is working");
+}
