78ea5f5f610f2a48b6b575a723b35dcca620addc
prathgan <prathamgandhi4@gmail.com>
2017-12-14 22:40:43 -0500

    revamped index ui + org structure

--- js/login.js
+++ js/login.js
@@ -1,25 +1,17 @@
 function login(){
+    document.getElementById("error").innerHTML = "Sorry, our servers are currently down. Please try again later."
+    /*
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-    console.log(email);
-    console.log(password);
     var loginDone=true;
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    console.log("works");
-    if(error){
-        console.log("login failed")
-        var errorCode = error.code;
         var errorMessage = error.message;
         document.getElementById("error").innerHTML = errorMessage;
         loginDone=false;
-    }
     });
     if(loginDone){
         document.getElementById("error").innerHTML = " ";
-        console.log("logged in");
         window.location.assign("../html/profile.html");
     }
-}
-function testFunc(){
-    console.log("js is working");
+    */
 }
--- js/register.js
+++ js/register.js
@@ -1,34 +1,31 @@
 function register(){
+    document.getElementById("fireError").innerHTML = "Sorry, our servers are currently down."
+    document.getElementById("error").innerHTML = "Please try again later."
+    /*
     console.log("func working")
     var email = document.getElementById('email').value;
     var pOne = document.getElementById('password').value;
     var pTwo = document.getElementById('confirm_password').value;
     var checked = document.getElementById('agree').checked;
-    console.log(email);
-    console.log(pOne);
-    console.log(pTwo);
-    console.log(checked)
     if(pOne!=pTwo){
-        console.log("not equal")
         document.getElementById("error").innerHTML = "Passwords did not match. Please try again.";
     }
     if(checked==false){
         document.getElementById("errorTwo").innerHTML = "You must agree to the Terms and Conditions to continue"
     }
     else if((pOne==pTwo)&&checked==true){
-        console.log("all good!")
         document.getElementById("error").innerHTML = " ";
         document.getElementById("errorTwo").innerHTML = " ";
+        var regSec = true;
         firebase.auth().createUserWithEmailAndPassword(email, pOne).catch(function(error) {
-            if(error){
-                var errorCode = error.code;
                 var errorMessage = error.message;
-                document.getElementById(fireError).innerHTML = errorMessage;
-            }
+                document.getElementById("fireError").innerHTML = errorMessage;
+                regSec=false;
+                console.log(errorMessage);
         });
-        window.location.assign("../html/profile.html");
-        console.log("on new page");
-        console.log(email);
-        console.log(pOne);
+        if(regSec){
+            window.location.assign("../html/login.html");
+        }
     }
+    */
 }
